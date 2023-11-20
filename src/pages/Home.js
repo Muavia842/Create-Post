09c033846e4fef89cfe25b7a0f5688f9b2e01f91
muavia haidri <muavia00842@gmail.com>
2023-11-21 00:28:59 +0500

    some changes in homeScreen page

---
+++ src/pages/Home.js
@@ -0,0 +1,61 @@
+import { collection, deleteDoc, doc, getDocs } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { auth, db } from '../firebase-config';
+import { RiDeleteBinLine } from 'react-icons/ri';
+function Home({ IsAuth }) {
+  const [postLists, setPostList] = useState([]);
+  const postsCollectionRef = collection(db, 'posts');
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postsCollectionRef);
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getPosts();
+  });
+
+  const deletePost = async (id) => {
+    const postDoc = doc(db, 'posts', id);
+    await deleteDoc(postDoc);
+  };
+  return (
+    <div className="homePage">
+      {postLists.map((post) => {
+        return (
+          <div className="post">
+            <div className="postHeader">
+              <div
+                style={{ display: 'flex', alignItems: 'center', gap: '15px' }}
+              >
+                <img
+                  style={{
+                    width: '60px',
+                    height: '60px',
+                    borderRadius: '300px',
+                  }}
+                  src={post.author.photo}
+                />
+                <h3>{post.author.name}</h3>
+              </div>
+              <div className="deletePost">
+                {IsAuth &&
+                  auth.currentUser &&
+                  post.author.id === auth.currentUser.uid && (
+                    <button onClick={() => deletePost(post.id)}>
+                      <RiDeleteBinLine />
+                    </button>
+                  )}
+              </div>
+            </div>
+            <div className="title">
+              <h1>{post.title}</h1>
+            </div>
+            <div className="postTextContainer">{post.postText}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/pages/Login.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { auth, provider } from '../firebase-config';
+// import { signInWithPopup } from "firebase/auth";
+import { useNavigate } from 'react-router-dom';
+import { signInWithPopup } from 'firebase/auth';
+function Login({ setIsAuth }) {
+  const navigate = useNavigate();
+  const signInwithGoogle = () => {
+    signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem('IsAuth', true);
+      setIsAuth(true);
+      navigate('/');
+    });
+  };
+  return (
+    <div className="loginPage">
+      <h2
+        style={{
+          color: 'white',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          gap: '10px',
+        }}
+      >
+        Welcome to Create-Post <span>Muavia Haidri</span>
+      </h2>
+      {/* <h1>Sign In With Google to Continue</h1> */}
+      <button className="login-with-google-btn" onClick={signInwithGoogle}>
+        Sign in With Google
+      </button>
+    </div>
+  );
+}
+
+export default Login;
