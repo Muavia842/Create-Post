527e96e6ee0e54ea09378b158c0dab50fdb2b496
muavia haidri <muavia00842@gmail.com>
2023-11-21 12:01:56 +0500

    some changes in our code

--- src/App.js
+++ src/App.js
@@ -58,3 +58,75 @@ function App() {
 }
 
 export default App;
+
+// import './App.css';
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Link,
+//   useNavigate,
+// } from 'react-router-dom';
+// import Home from './pages/Home';
+// import CreatePost from './pages/CreatePost';
+// import Login from './pages/Login';
+// import { useState } from 'react';
+// import { signOut } from 'firebase/auth';
+// import { auth } from './firebase-config';
+
+// function App() {
+//   const [IsAuth, setIsAuth] = useState(
+//     localStorage.getItem('IsAuth') === 'true'
+//   );
+//   const navigate = useNavigate();
+
+//   const singUserOut = () => {
+//     signOut(auth).then(() => {
+//       localStorage.clear();
+//       setIsAuth(false);
+//       navigate('/login');
+//     });
+//   };
+
+//   return (
+//     <div className="appMain">
+//       <Router>
+//         <nav className="navMain">
+//           <Link to="/">Home</Link>
+
+//           {!IsAuth ? (
+//             <Link to="/login">Login</Link>
+//           ) : (
+//             <>
+//               <Link to="/createpost">Create Post</Link>
+//               <button
+//                 onClick={singUserOut}
+//                 style={{ padding: '7px', borderRadius: '5px', border: 'none' }}
+//               >
+//                 Log Out
+//               </button>
+//               {auth.currentUser && auth.currentUser.photoURL && (
+//                 <img
+//                   style={{
+//                     width: '50px',
+//                     height: '50px',
+//                     borderRadius: '50%',
+//                   }}
+//                   src={auth.currentUser.photoURL}
+//                   alt="User Profile"
+//                 />
+//               )}
+//             </>
+//           )}
+//         </nav>
+//         <Routes>
+//           <Route path="/" element={<Home IsAuth={IsAuth} />} />
+//           <Route path="/createpost" element={<CreatePost IsAuth={IsAuth} />} />
+//           <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
+//         </Routes>
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
